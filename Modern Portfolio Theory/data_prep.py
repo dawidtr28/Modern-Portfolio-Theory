65092848bbc58609d12ce4d05f2e22eb18780dda
dawidtr28 <dawidtryka1@gmail.com>
2025-10-17 12:45:05 +0200

    Use trading_days for annual calculations

--- Modern Portfolio Theory/data_prep.py
+++ Modern Portfolio Theory/data_prep.py
@@ -1,10 +1,13 @@
 import numpy as np
 import yfinance as yf
 
+from data_prep import trading_days
+
 def get_data(tickers, start_date, end_date):
     data = yf.download(tickers, start=start_date, end=end_date)['Close']
     log_returns = np.log(data / data.shift(1)).dropna()
-    mean_returns_annual = log_returns.mean() * 252
-    cov_matrix_annual = log_returns.cov() * 252
+    mean_returns_annual = log_returns.mean() * trading_days
+    cov_matrix_annual = log_returns.cov() * trading_days
+
 
     return mean_returns_annual, cov_matrix_annual
